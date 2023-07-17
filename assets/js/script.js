679b8d95c46cf2ee51935608e2df3a05d771fbae
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-07-17 20:13:21 +0300

    add: active states

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-// let menuOpen = document.getElementById('menu-open')
-// let menuClose = document.getElementById('menu-close')
+let body = document.querySelector('body')
+let menuOpen = document.getElementById('menu-open')
+let menuClose = document.getElementById('menu-close')
+let navList = document.querySelector('ul')
 
-// menuOpen.addEventListener('click', function(){
-//       menuOpen.style.display = 'none'
-//       menuClose.style.display = 'block'
-// })
+menuOpen.addEventListener('click', function(){
+      menuOpen.style.display = 'none'
+      menuClose.style.display = 'block'
 
-// menuClose.addEventListener('click', function(){
-//       menuClose.style.display = 'none'
-//       menuOpen.style.display = 'block'
-// })
+      if (navList.style.display === 'block')
+            navList.style.display = 'none'
+      else
+            navList.style.display = 'block'
+})
+
+menuClose.addEventListener('click', function(){
+      menuClose.style.display = 'none'
+      menuOpen.style.display = 'block'
+
+      if (navList.style.display === 'block')
+            navList.style.display = 'none'
+      else
+            navList.style.display = 'block'
+})

d9e9800925f1c8847726114896d4f0381f895d03
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-07-17 17:48:35 +0300

    fix: nav bar display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
-let menuOpen = document.getElementById('menu-open')
-let menuClose = document.getElementById('menu-close')
+// let menuOpen = document.getElementById('menu-open')
+// let menuClose = document.getElementById('menu-close')
 
-menuOpen.addEventListener('click', function(){
-      menuOpen.style.display = 'none'
-      menuClose.style.display = 'block'
-      let nav = document.querySelector('nav')
-      nav.classList.toggle('active')
-})
+// menuOpen.addEventListener('click', function(){
+//       menuOpen.style.display = 'none'
+//       menuClose.style.display = 'block'
+// })
 
-menuClose.addEventListener('click', function(){
-      menuClose.style.display = 'none'
-      menuOpen.style.display = 'block'
-      let nav = document.querySelector('nav')
-      nav.classList.toggle('active')
-})
+// menuClose.addEventListener('click', function(){
+//       menuClose.style.display = 'none'
+//       menuOpen.style.display = 'block'
+// })
